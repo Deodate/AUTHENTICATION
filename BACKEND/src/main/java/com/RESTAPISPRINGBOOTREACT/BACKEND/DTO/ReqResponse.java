d405051b67990f290a167af05fcf0eac7d123674
Deodate <ajadeo04@gmail.com>
2024-12-10 20:01:34 +0200

    Progress

---
+++ BACKEND/src/main/java/com/RESTAPISPRINGBOOTREACT/BACKEND/DTO/ReqResponse.java
@@ -0,0 +1,31 @@
+package com.RESTAPISPRINGBOOTREACT.BACKEND.DTO;
+
+import java.util.List;
+
+import com.RESTAPISPRINGBOOTREACT.BACKEND.entity.Userstbl;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ReqResponse {
+
+    private int statusCode;
+    private String error;
+    private String message;
+    private String token;
+    private String refreshToken;
+    private String expiration;
+    private String fullName;
+    private String nationality;
+    private String position;
+    private String email;
+    private String password;
+    private Userstbl userstbl;
+    private List<Userstbl> userstblList;
+
+    
+}
